2026e8c3fbc9d35c1db75afc65691ae40795ff05
lucarebuffi <lrebuffi@anl.gov>
2019-08-07 10:17:08 -0500

    Debug

---
+++ wofryshadow/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofryshadow SETUP.PY
+short_version = '1.0.2'
+version = '1.0.2'
+full_version = '1.0.2.dev0+a7beb00'
+git_revision = 'a7beb0058353a64d3b62ca025d4ecd7c38a5f340'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
